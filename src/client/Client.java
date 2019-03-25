263f3a5a57cb5d49b0a16c53fa65d9377a68130e
Michael <micoraafat96@gmail.com>
2019-03-25 16:16:49 +0200

    .

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-
 public class Client {
 	public static void main(String args[]) throws IOException {
         String address = args[0];
